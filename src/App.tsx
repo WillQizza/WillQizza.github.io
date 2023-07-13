d3c03ff5ea0762b8ad77e94d60028d0848183191
WillQizza <williamqipizza@gmail.com>
2023-07-13 09:27:37 -0700

    add view my projects hint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.scss';
 import Landing from "./sections/Landing";
+import NavigationBar from './sections/Nav';
 
 function App() {
   return (
     <>
+      <NavigationBar />
       <Landing />
     </>
   );
--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,5 +1,7 @@
-import { Component } from "react";
+import { Component, UIEvent } from "react";
 import "./Landing.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default class Landing extends Component {
 
@@ -9,7 +11,14 @@ export default class Landing extends Component {
                 <h1>Hey, I'm William!</h1>
                 <h2>Student Software Developer</h2>
                 <div id="landing-tagline-container">
-                    <p>I develop full-stack applications, and I love it.</p>
+                    <p>I develop full-stack applications and I love it.</p>
+                </div>
+            </div>
+
+            <div id="landing-scroll-down-hint-container">
+                <div>
+                    View my Projects<br />
+                    <FontAwesomeIcon icon={faAngleDown} />
                 </div>
             </div>
         </div>
