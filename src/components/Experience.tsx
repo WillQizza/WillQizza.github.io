c1c8bcf01b6b263d3d1287f2ff8576ef3cab4804
WillQizza <williamqipizza@gmail.com>
2023-07-16 21:31:23 -0600

    experiences (not mobile ready)

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -19,9 +19,13 @@ type ExperienceProperties = {
 export default class Experience extends Component<ExperienceProperties> {
 
     render() {
+        const endDate = this.props.to.year && this.props.to.month ? `${this.props.to.month} ${this.props.to.year}` : "Present";
         return <div className="experience">
-            <h3>{ this.props.company } | { this.props.location }</h3>
-            <i>{ this.props.title }</i><br /><br />
+            <h3>{ this.props.company }</h3>
+            <i>{ this.props.title }</i><br />
+            <span>
+                { this.props.from.month } { this.props.from.year } - { endDate }
+            </span>
             <ul>
                 { this.props.responsibilities.map(responsibility => <li>{ responsibility }</li>) }
             </ul>
