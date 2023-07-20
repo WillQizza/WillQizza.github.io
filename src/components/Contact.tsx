f07168c428c014505830fed90a7e9e5d6d275b2b
WillQizza <williamqipizza@gmail.com>
2023-07-20 15:36:31 -0700

    fix mobile sizing issue

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,7 +10,7 @@ export default class Contact extends Component {
             <hr />
             <div id="contact-content">
                 <div id="contact-avatar">
-                    <img alt="William Qi" src="https://avatars.githubusercontent.com/u/20137336" width="192" height="192" />
+                    <img alt="William Qi | Software Developer" src="https://avatars.githubusercontent.com/u/20137336" width="192" height="192" />
                 </div>
                 <p>Hello there! I'm William, a passionate third-year Computing Science student at the University of Alberta. Since the age of 9, I've been captivated by programming, and my love for coding has only grown stronger over the years.</p>
                 <p>Whether it's building applications that enhance user experiences, developing software to address real-world problems, or creating innovative solutions to make everyday tasks more efficient, I find the process of coding and its potential to create meaningful change thrilling!</p>
