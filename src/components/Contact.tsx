2034fac70b9eba850fc9222c247e13b7c0aa6f00
WillQizza <williamqipizza@gmail.com>
2023-07-20 19:18:25 -0600

    add socials

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,7 @@
 import { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import "./Contact.scss";
 
@@ -16,6 +19,11 @@ export default class Contact extends Component {
                 <p>Whether it's building applications that enhance user experiences, developing software to address real-world problems, or creating innovative solutions to make everyday tasks more efficient, I find the process of coding and its potential to create meaningful change thrilling!</p>
                 <p>Although, when I'm not immersed in coding, you'll likely find me indulging in papercraft, getting lost in a good book, cosplaying as some of my favorite characters, or just exploring the vast world wide web.</p>
                 <p>Let's connect and create a better tomorrow together!</p>
+                <div id="contact-socials">
+                    <a href="mailto:williamqi.250@gmail.com"><FontAwesomeIcon className="contact-social" icon={faEnvelope} /></a> 
+                    <a href="https://github.com/willqizza/" target="_blank" rel="noreferrer"><FontAwesomeIcon className="contact-social" icon={faGithub} /> </a>
+                    <a href="https://www.linkedin.com/in/william-qi/" target="_blank" rel="noreferrer"><FontAwesomeIcon className="contact-social" icon={faLinkedin} /></a>
+                </div>
             </div>
         </div>
     }
