e07655eb4f696ed76c9153c5ea364103f8dd3693
git-qiw <williamqi@vanmok.com>
2023-07-28 09:27:15 -0700

    fix project placeholder count being improperly calculated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,13 +10,13 @@ const IDEAL_COLUMNS = 3;
 export default class Projects extends Component<SectionProperties> {
 
     render() {
-        
+        const placeholderProjectCount = projects.length % IDEAL_COLUMNS > 0 ? 3 - (projects.length % IDEAL_COLUMNS) : 0;
         return <div id="projects-container" ref={this.props.waypoint}>
             <h1>Projects</h1>
             <hr />
             <div id="projects">
                 { projects.map(project => <Project key={project.repository} name={project.name} description={project.description} skills={project.skills} repository={project.repository} />) }
-                { Array.from({ length: (projects.length - 1) % IDEAL_COLUMNS }).map((_, index) => <div key={index} className="project placeholder"></div>) }
+                { Array.from({ length: placeholderProjectCount }).map((_, index) => <div key={index} className="project placeholder"></div>) }
             </div>
         </div>
     }
