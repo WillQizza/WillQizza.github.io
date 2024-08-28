93ff4b0b56f972aa3d6acdd6bedbeeacf376fd10
WillQizza <williamqipizza@gmail.com>
2024-08-28 10:54:02 -0600

    new experience + devops

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -17,7 +17,7 @@ export default class Landing extends Component<LandingProperties> {
                 <h1>Hey, I'm William</h1>
                 <h2>Student Software Developer</h2>
                 <div id="landing-tagline-container">
-                    <p>I develop full-stack applications and I love it!</p>
+                    <p>DevOps-driven, full-stack developer creating streamlined solutions</p>
                 </div>
             </div>
 
