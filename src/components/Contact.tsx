9763439c5b6c92d8e3ff838e42cf36014e993bde
WillQizza <williamqipizza@gmail.com>
2024-09-28 01:27:08 -0600

    update for y4

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ export default class Contact extends Component<SectionProperties> {
                         <div id="contact-avatar">
                             <img alt="William Qi | Software Developer" src="./me.jpg" width="192" height="192" />
                         </div>
-                        <p>Hello there! I'm William, a passionate third-year Computing Science student at the University of Alberta. Since the age of 9, I've been captivated by programming, and my love for coding has only grown stronger over the years.</p>
+                        <p>Hello there! I'm William, a passionate fourth-year Computing Science student at the University of Alberta graduating this April. Since the age of 9, I've been captivated by programming, and my love for coding has only grown stronger over the years.</p>
                         <p>Whether it's building applications that enhance user experiences, developing software to address real-world problems, or creating innovative solutions to make everyday tasks more efficient, I find the process of coding and its potential to create meaningful change thrilling!</p>
                         <p>Although, when I'm not immersed in coding, you'll likely find me indulging in papercraft, getting lost in a good book, cosplaying as some of my favorite characters, or just exploring the vast world wide web.</p>
                         <p>Let's connect and create a better tomorrow together!</p>
